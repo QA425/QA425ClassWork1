5e09a33d97e36031fa990541cc9c687dbd580041
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-12-29 12:36:37 +0300

    Обновлена тема файлы.

--- module_06_01_files/files_06_04.py
+++ module_06_01_files/files_06_04.py
@@ -15,9 +15,10 @@ def write_user_data(filename, user_data):
     return 'Данные успешно записаны!'
 
 
-my_filename = r'files\user_answers.txt'
+user_name = input('Введите ваше имя: ')
 user_language = input('Какой язык вы изучаете: ')
 user_time = input('Как долго: ')
 user_institution = input('Где: ')
+my_filename = fr'files\{user_name}_answers.txt'
 user_data_list = [user_language, user_time, user_institution]
 print(write_user_data(my_filename, user_data_list))
